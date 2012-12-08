2237895c84c93dedd77730848c665d92b66ac42d
Igor Dvorkin (idvorkin <Igor Dvorkin (idvorkin@gmail.com)>
2012-12-07 21:50:23 -0800

    Adding missing file :)

---
+++ UnitTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace PerformanceMeasurement
+{
+    [TestFixture]
+    internal class UnitTests
+    {
+        [TestCase]
+        public void TestMeasureActionDoesNotCrash()
+        {
+            var stats = LinqPadUX.ComputerSpecs;
+            var timeIt = LinqPadUX.Measure.Action(() => "Hello".GetHashCode());
+        }
+        [TestCase]
+        public void TestDefaultBenchmarkToWebBrowserDoesNotCrash()
+        {
+            LinqPadUX.DefaultBenchmarkToWebBrowser();
+        }
+    }
+}
